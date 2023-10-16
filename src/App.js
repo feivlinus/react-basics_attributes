2725313237b56d1829b6dc08b0081837bb2eb6e0
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-16 13:01:50 +0200

    Changed the return of the function App()

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return HelloWorldThing();
+  return (
+    <div>
+      <HelloWorldThing />
+      <HelloWorldThing />
+    </div>
+  );
 }
 
 function HelloWorldThing() {
@@ -10,7 +15,7 @@ function HelloWorldThing() {
     <article className="article">
       <h2 className="article__title">Hello World Thing</h2>
       <label htmlFor="inputField">This is some Input righ here:</label>
-      <input id="inputField"></input>
+      <input id="inputField" name="inputField"></input>
       <a
         className="article__link"
         href="https://en.wikipedia.org/wiki/%22Hello,_World!%22_program"
