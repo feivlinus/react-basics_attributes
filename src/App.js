e32dd76d9de58d8ab1502d0d5e345bc28ca9f1d7
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-16 11:29:50 +0200

    Challenge completed

--- src/App.js
+++ src/App.js
@@ -2,5 +2,21 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with an article</div>;
+  return HelloWorldThing();
+}
+
+function HelloWorldThing() {
+  return (
+    <article className="article">
+      <h2 className="article__title">Hello World Thing</h2>
+      <label htmlFor="inputField">This is some Input righ here:</label>
+      <input id="inputField"></input>
+      <a
+        className="article__link"
+        href="https://en.wikipedia.org/wiki/%22Hello,_World!%22_program"
+      >
+        Hello World
+      </a>
+    </article>
+  );
 }
